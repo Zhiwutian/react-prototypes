71951c3ca2a798cbfe769715bd4248da5de057e1
zhiwutian <brettalbright@outlook.com>
2018-04-24 17:54:59 -0700

    First React Prototype completed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+
+function luckyNumber () {
+    const ranNumber = Math.floor((Math.random() * 1000) + 1);
+    return ranNumber;
+}
+
+function greeting (user) {
+    return <div className="container"><h1>Welcome {user.name}!</h1><h2 className="text-muted">Your lucky number is {user.luckyNumber}</h2></div>
+
+}
+
+const user = {
+    "name" : "Rhysin",
+    "luckyNumber" : luckyNumber()
+};
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(user),
     document.getElementById('root')
 );
