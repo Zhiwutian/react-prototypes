a167281132c0579ced6b161baf895437391c8058
zhiwutian <brettalbright@outlook.com>
2018-04-27 17:19:00 -0700

    completed react functional prototypes

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,27 @@
+import React from "react";
+import Table from "./table";
+
+const students = [{
+    name: "Jimmy John",
+    course: "Double Names",
+    grade: 93
+},{
+    name: "John Kistler",
+    course: "Engineering",
+    grade: 99
+},{
+    name: "Bora Arcuna",
+    course: "Medicine",
+    grade: 99
+}];
+
+function App () {
+    return <div className="container">
+        <h1>Student Grade Table</h1>
+        <Table data={students}/>
+        </div>;
 
 
+}
+
+export default App;
